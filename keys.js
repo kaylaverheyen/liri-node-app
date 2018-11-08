ab5ea176c82b3957afbc3035477c94bbaf52ff22
kaylaverheyen <kaylaverheyen@icloud.com>
2018-11-07 23:27:36 -0600

    structure

---
+++ keys.js
@@ -0,0 +1,12 @@
+console.log('this is loaded');
+
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+};
+exports.bandsInTown = {
+    id: process.env.BIT_ID
+};
+exports.omdb = {
+    id: process.env.OMDB_ID
+};
---
+++ liri.js
@@ -0,0 +1,9 @@
+//read and set variables with dotenv
+require("dotenv").config();
+
+
+// if (result.error) {
+//     throw result.error
+//   }
+
+//   console.log(result.parsed)
